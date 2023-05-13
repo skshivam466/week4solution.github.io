08166fa417eda5d26a1093d6bbe9f6dd00678abf
Shivam Kumar Sah <skshivam466@gmail.com>
2023-05-13 19:00:02 +0545

    first commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
-//To fix the code to properly handle error cases
-function getName(name) {
-    if (typeof name === 'undefined') {
-      throw new Error('Name is undefined');
-    }
-    return name.trim();
+// TODO: STEP 0 - Define an array of category objects
+var categories = [
+  {
+    short_name: "L",
+    name: "Lunch",
+    special_instructions: "Available only during lunch hours"
+  },
+  {
+    short_name: "D",
+    name: "Dinner",
+    special_instructions: "Available only during dinner hours"
+  },
+  {
+    short_name: "S",
+    name: "Sushi",
+    special_instructions: "Available all day"
+  },
+  {
+    short_name: "A",
+    name: "Appetizers",
+    special_instructions: "Available all day"
+  },
+  {
+    short_name: "B",
+    name: "Beverages",
+    special_instructions: "Available all day"
   }
-  
+];
 
-  function sayHello(name) {
-    name = getName(name);
-    console.log('Hello ' + name);
-  }
-  
-  function sayGoodbye(name) {
-    name = getName(name);
-    console.log('Goodbye ' + name);
-  }
-  
-  // To print hello or goodbye
-  function printGreetings(names) {
-    for (var i = 0; i < names.length; i++) {
-      var firstLetter = names[i].charAt(0);
-      if (firstLetter.toLowerCase() === 'j') {
-        sayGoodbye(names[i]);
-      } else {
-        sayHello(names[i]);
-      }
-    }
-  }
-  
-  // Test the function with an array of names
-  var names = ['John', 'Jane', 'Jim'];
-  printGreetings(names);
-  
+// TODO: STEP 1 - Randomly select a category short_name from the categories array and assign it to the variable "randomCategoryShortName"
+var randomCategoryShortName = categories[Math.floor(Math.random() * categories.length)].short_name;
+
+// TODO: STEP 2 - Update the onclick function for the "Specials" tile to use the randomCategoryShortName variable
+document.getElementById("specials").onclick = function() {
+  $dc.loadMenuItems(randomCategoryShortName);
+};
+
+// TODO: STEP 3 - Define a function to generate the HTML for a single category page based on a category object
+function getCategoryHtml(category) {
+  var html = "<h2>" + category.name + "</h2>";
+  html += "<p>" + category.special_instructions + "</p>";
+  html += "<ul>";}
+  for (var i = 0; i < $dc.getAllMenuItems().menu_items.length; i++) {
+    var menuItem = $dc.getAllMenuItems().menu_items[i];
+    if (menuItem.short_name.startsWith(category.short_name));}
